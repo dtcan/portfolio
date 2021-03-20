0fbff86c51b9f161a04fdd9d2feba93c56e06e6f
David Canagasabey <david@dtcan.dev>
2021-03-20 16:59:53 -0400

    Front page bullet points and contact info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,4 +25,13 @@
 
 <h1><TypedText text="david canagasabey"></TypedText></h1>
 
-<p>Some information about me goes here.</p>
+<p>
+	Full-stack web developer, also familiar with low-level and functional programming<br />
+	Strong theoretical background (Computer Science Specialist & Math Major @ UofT)<br />
+	Always excited to learn new skills
+</p>
+<p>
+	Email: <a target="blank" href="mailto:david@dtcan.dev">david@dtcan.dev</a><br />
+	Phone: 647.336.2595<br />
+	Github: <a target="blank" href="https://github.com/dtcan">dtcan</a>
+</p>
